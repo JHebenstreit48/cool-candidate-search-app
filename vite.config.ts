e9bc289f00c3f60da9c19675e4e0fadd7ac7bede
Justin <j.hebenstreit25@gmail.com>
2024-09-10 16:44:02 -0500

    fixed .env issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: './env',
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  }
 })
