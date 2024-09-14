fd02a8113db668c1cf71d1fe927189c64d05cb70
Justin <j.hebenstreit25@gmail.com>
2024-09-14 09:54:36 -0500

    saving recent changes.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
 
-  // TODO: Add necessary code to display the navigation bar and link between the pages
-
   export default function Navigation() {
     const currentPage = useLocation().pathname;
   
@@ -10,8 +8,7 @@ import { Link, useLocation } from "react-router-dom";
         <li className="nav-item">
           <Link
             to="/"
-            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+
             className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
           >
             Home
@@ -20,7 +17,7 @@ import { Link, useLocation } from "react-router-dom";
         <li className="nav-item">
           <Link
             to="/savedcandidates"
-            // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+
             className={currentPage === '/savedcandidates' ? 'nav-link active' : 'nav-link'}
           >
             Potential Candidates
