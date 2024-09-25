05eead8e7a7c0fa69759c1874c9eb91a3f30e16e
Justin <j.hebenstreit25@gmail.com>
2024-09-25 18:11:11 -0500

    worked on Candidate Search page

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,75 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type { Candidate } from '../interfaces/Candidate.interface';
+
 
 const CandidateSearch = () => {
-  return <h1>Candidate Search</h1>;
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [username, setUsername] = useState<Candidate>({
+    id: null,
+    login: null,
+    email: null,
+    html_url: null,
+    name: null,
+    avatar_url: null,
+    bio: null,
+    company: null,
+    location: null,
+    blog: null,
+  });
+
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const gitHubCandidateSearch = async (username: string | null) => {
+    const gitHubAPIData = await searchGithubUser(username);
+    console.log(gitHubAPIData);
+    setUsername(gitHubAPIData);
+  };
+
+  const gitHubSearch = async () => {
+    const gitHubAPIData = await searchGithub();
+    console.log(gitHubAPIData);
+    setCandidates(gitHubAPIData);
+  };
+
+  const userData = async (currentCandidate: boolean) => {
+    if (currentCandidate) {
+      let candidateList: Candidate[] = [];
+      let storedCandidate = localStorage.getItem('candidate');
+      if (typeof storedCandidate === 'string') {
+        candidateList = JSON.parse(storedCandidate);
+      }
+      candidateList.push(username);
+      localStorage.setItem('candidate', JSON.stringify(candidateList));
+    }
+    if (currentIndex + 1 < candidates.length) {
+      setCurrentIndex(currentIndex + 1);
+      await gitHubCandidateSearch(candidates[currentIndex + 1].login);
+    } else {
+      setCurrentIndex(0);
+      await searchGithub();
+    }
+  }
+
+  useEffect(() => {
+    gitHubCandidateSearch(currentUser.login || "")
+    gitHubSearch();
+  }, []);
+
+  return (
+
+    <div>
+
+      <h1>Candidate Search</h1>
+
+      
+
+    </div>
+
+  )
+
+
+    ;
 };
 
 export default CandidateSearch;
