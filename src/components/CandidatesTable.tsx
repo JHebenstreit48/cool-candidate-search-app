e68e49ca9cb682008a19b16ced1316769613b3a0
Justin <j.hebenstreit25@gmail.com>
2024-10-19 21:26:09 -0500

    working on table CSS for deployment

--- src/components/CandidatesTable.tsx
+++ src/components/CandidatesTable.tsx
@@ -29,38 +29,43 @@ function SavedCandidatesList() {
     return (
         <>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Location</th>
-                        <th>Avatar</th>
-                        <th>E-Mail</th>
-                        <th>GitHub</th>
-                        <th>Company</th>
-                        <th>Bio</th>
-                        <th>Remove</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {savedCandidates.map((candidateInfo, index) =>
-                        <tr key={candidateInfo.name ?? '' + index}>
-                            <td className="data">{candidateInfo.name}</td>
-                            <td className="data">{candidateInfo.login}</td>
-                            <td className='data'>{candidateInfo.location}</td>
-                            <td><img className="savedCandidatesAvatar" src={candidateInfo.avatar_url!} alt={candidateInfo.name ?? ''} /></td>
-                            <td className="data">{candidateInfo.email}</td>
-                            <td className="data">{candidateInfo.html_url}</td>
-                            <td className="data">{candidateInfo.company}</td>
-                            <td className="data">{candidateInfo.bio}</td>
-                            <td>
-                                <button className="fa-solid fa-circle-minus" onClick={() => removeCandidate(index)}></button>
-                            </td>
+            <div >
+
+                <table className='candidatesTable'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Location</th>
+                            <th>Avatar</th>
+                            <th>E-Mail</th>
+                            <th>GitHub</th>
+                            <th>Company</th>
+                            <th>Bio</th>
+                            <th>Remove</th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {savedCandidates.map((candidateInfo, index) =>
+                            <tr key={candidateInfo.name ?? '' + index}>
+                                <td className="data">{candidateInfo.name}</td>
+                                <td className="data">{candidateInfo.login}</td>
+                                <td className='data'>{candidateInfo.location}</td>
+                                <td><img className="savedCandidatesAvatar" src={candidateInfo.avatar_url!} alt={candidateInfo.name ?? ''} /></td>
+                                <td className="data">{candidateInfo.email}</td>
+                                <td className="data">{candidateInfo.html_url}</td>
+                                <td className="data">{candidateInfo.company}</td>
+                                <td className="data">{candidateInfo.bio}</td>
+                                <td>
+                                    <button className="fa-solid fa-circle-minus" onClick={() => removeCandidate(index)}></button>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+
+            </div>
+
 
         </>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './CSS/Page.css';
-
 import App from './App.tsx';
 import CandidateSearch from './pages/CandidateSearch.tsx';
 import SavedCandidates from './pages/SavedCandidates.tsx';
